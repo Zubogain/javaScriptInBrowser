6599375d088f81b1901446236893e81a612c0cc6
Zubogain <zubogain@gmail.com>
2018-03-28 18:45:49 +0600

    Refactoring

--- html-document-structure/tabs/js/tabs.js
+++ html-document-structure/tabs/js/tabs.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const MAIN = document.getElementById('tabs');
 	const NAV = MAIN.querySelector('.tabs-nav');
-	const tabs = MAIN.querySelector('.tabs-content');
-	let navClone = NAV.children[0].cloneNode(true);
-	NAV.removeChild(NAV.children[0]);
+	const TABS = MAIN.querySelector('.tabs-content');
+	const NAV_CLONE = NAV.children[0];
+	NAV.removeChild(NAV_CLONE);
 
-	for (let i = 0; i < tabs.children.length; i++) {
-		const title = tabs.children[i].dataset.tabTitle;
-		const icon = tabs.children[i].dataset.tabIcon;
+	for (let i = 0; i < TABS.children.length; i++) {
+		const title = TABS.children[i].dataset.tabTitle;
+		const icon = TABS.children[i].dataset.tabIcon;
 
-		navClone.children[0].removeAttribute('class');
-		navClone.children[0].classList.add(icon);
-		navClone.children[0].textContent = title;
+		NAV_CLONE.children[0].removeAttribute('class');
+		NAV_CLONE.children[0].classList.add(icon);
+		NAV_CLONE.children[0].textContent = title;
 
-		NAV.appendChild(navClone.cloneNode(true)).addEventListener('click', currentTab);
-
+		NAV.appendChild(NAV_CLONE.cloneNode(true)).addEventListener('click', currentTab);
 	}
+	
 	currentTab.call(NAV.children[0]);
 
 	function currentTab() {
 		for (let i = 0; i < this.parentElement.children.length; i++) {
 			this.parentElement.children[i].classList.remove('ui-tabs-active');
-			tabs.children[i].classList.add('hidden');
-			if(tabs.children[i].dataset.tabTitle === this.children[0].textContent) {
-				tabs.children[i].classList.remove('hidden');
+			TABS.children[i].classList.add('hidden');
+			if(TABS.children[i].dataset.tabTitle === this.children[0].textContent) {
+				TABS.children[i].classList.remove('hidden');
 			}
 		}
 		this.classList.add('ui-tabs-active');
